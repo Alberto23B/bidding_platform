68f4aab13079f1901b1f22d98d79e94fdabbf585
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-27 12:51:36 +0100

    Create basic calculateBasePrice function

---
+++ server/lib/calculateBasePrice.js
@@ -0,0 +1,27 @@
+export const calculateBasePrice = (
+  base,
+  minStep,
+  maxStep,
+  threshold,
+  steps
+) => {
+  let newBasePrice = base;
+  const operator = Math.sign(steps);
+  const repetitions = Math.abs(steps);
+
+  for (let i = 0; i < repetitions; i++) {
+    if (newBasePrice < threshold) {
+      newBasePrice += minStep * operator;
+    } else if (newBasePrice > threshold) {
+      newBasePrice += maxStep * operator;
+    } else {
+      if (operator === 1) {
+        newBasePrice += maxStep * operator;
+      } else {
+        newBasePrice += minStep * operator;
+      }
+    }
+  }
+
+  return newBasePrice;
+};
